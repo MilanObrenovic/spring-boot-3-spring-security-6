ffcf2558ddf77701cd03f3272e052ca924905ef9
Milan Obrenovic <mmobrenovic@gmail.com>
2023-01-30 23:37:22 +0100

    Transform the User to an entity

--- src/main/java/com/example/security/user/User.java
+++ src/main/java/com/example/security/user/User.java
@@ -1,20 +1,44 @@
 package com.example.security.user;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.*;
+import org.hibernate.Hibernate;
 
-@Data
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "\"user\"")
 public class User {
 
+    @Id
+    @GeneratedValue
     private Integer id;
     private String firstName;
     private String lastName;
     private String email;
     private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        User user = (User) o;
+        return id != null && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 
 }
