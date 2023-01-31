a892ae2b4eb7c8a4d7692c1ae64e53fc3474697c
Milan Obrenovic <mmobrenovic@gmail.com>
2023-01-31 02:41:40 +0100

    Extract claims from JWT

--- src/main/java/com/example/security/service/JwtService.java
+++ src/main/java/com/example/security/service/JwtService.java
@@ -1,7 +1,10 @@
 package com.example.security.service;
 
+import io.jsonwebtoken.Claims;
+
 public interface JwtService {
 
     String extractUsername(String token);
+    Claims extractAllClaims(String token);
 
 }
--- src/main/java/com/example/security/serviceImpl/JwtServiceImpl.java
+++ src/main/java/com/example/security/serviceImpl/JwtServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.security.serviceImpl;
 
 import com.example.security.service.JwtService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,6 +11,16 @@ public class JwtServiceImpl implements JwtService {
     @Override
     public String extractUsername(String token) {
         return null;
+    }
+
+    @Override
+    public Claims extractAllClaims(String token) {
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
     }
 
 }
