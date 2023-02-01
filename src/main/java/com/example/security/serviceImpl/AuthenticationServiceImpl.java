e37b8507673d3698f1b768bc1886210955ea7e58
Milan Obrenovic <mmobrenovic@gmail.com>
2023-02-01 14:58:24 +0100

    Implement the register method

--- src/main/java/com/example/security/serviceImpl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/security/serviceImpl/AuthenticationServiceImpl.java
@@ -3,17 +3,41 @@ package com.example.security.serviceImpl;
 import com.example.security.dto.request.AuthenticationRequest;
 import com.example.security.dto.request.RegisterRequest;
 import com.example.security.dto.response.AuthenticationResponse;
+import com.example.security.enums.Role;
+import com.example.security.model.User;
+import com.example.security.repository.UserRepository;
 import com.example.security.service.AuthenticationService;
+import com.example.security.service.JwtService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class AuthenticationServiceImpl implements AuthenticationService {
 
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+
     @Override
     public AuthenticationResponse register(RegisterRequest request) {
-        return null;
+        User user = User
+                .builder()
+                .firstName(request.getFirstName())
+                .lastName(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
     }
 
     @Override
