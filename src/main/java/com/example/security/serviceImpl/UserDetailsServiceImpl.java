6c939a23a3afcc757e6257ad28a0df7ecee0a51c
Milan Obrenovic <mmobrenovic@gmail.com>
2023-02-01 17:29:12 +0100

    Create a demo controller

--- src/main/java/com/example/security/serviceImpl/UserDetailsServiceImpl.java
+++ src/main/java/com/example/security/serviceImpl/UserDetailsServiceImpl.java
@@ -1,15 +1,22 @@
 package com.example.security.serviceImpl;
 
+import com.example.security.repository.UserRepository;
 import com.example.security.service.UserDetailsService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
 
     @Override
     public UserDetails loadByUsername(String userEmail) {
-        return null;
+        return userRepository
+                .findByEmail(userEmail)
+                .orElseThrow();
     }
 
 }
