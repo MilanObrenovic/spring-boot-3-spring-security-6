4608d03d9197c732e7c18319d229c553ebde6574
Milan Obrenovic <mmobrenovic@gmail.com>
2023-02-01 15:01:28 +0100

    Implement the authenticate method

--- src/main/java/com/example/security/serviceImpl/AuthenticationServiceImpl.java
+++ src/main/java/com/example/security/serviceImpl/AuthenticationServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.security.serviceImpl;
 
+import lombok.RequiredArgsConstructor;
 import com.example.security.dto.request.AuthenticationRequest;
 import com.example.security.dto.request.RegisterRequest;
 import com.example.security.dto.response.AuthenticationResponse;
@@ -8,7 +9,8 @@ import com.example.security.model.User;
 import com.example.security.repository.UserRepository;
 import com.example.security.service.AuthenticationService;
 import com.example.security.service.JwtService;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -19,6 +21,7 @@ public class AuthenticationServiceImpl implements AuthenticationService {
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public AuthenticationResponse register(RegisterRequest request) {
@@ -42,7 +45,23 @@ public class AuthenticationServiceImpl implements AuthenticationService {
 
     @Override
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(
+                request.getEmail(),
+                request.getPassword()
+            )
+        );
+
+        User user = userRepository
+                .findByEmail(request.getEmail())
+                .orElseThrow();
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
     }
 
 }
