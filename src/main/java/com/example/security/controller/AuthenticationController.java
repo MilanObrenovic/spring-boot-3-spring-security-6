979832afd4bf233378127b644e274ce07e39d288
Milan Obrenovic <mmobrenovic@gmail.com>
2023-02-01 14:46:14 +0100

    Create the authentication controller

---
+++ src/main/java/com/example/security/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.example.security.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(
+        @RequestBody RegisterRequest request
+    ) {
+        // TODO: implement
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+        @RequestBody AuthenticationRequest request
+    ) {
+        // TODO: implement
+    }
+
+}
